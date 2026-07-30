b7c476be953033d1c6c5621bf126793725e928ab
Kuol Magok <heiskuol001@gmail.com>
2026-07-30 22:27:08 +0300

    feat: refactor user registration and login controllers with service integration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,10 @@
-import userService from "../services/user.service.js"
+import {userRegistrationService, userLoginService} from "../services/user.service.js"
 
 
-const userRegistration = async (req,res) => {
+const userRegistrationController = async (req,res) => {
     try {
         const { name, email, password, role, phone, location } = req.body
-        const user = await userService(
+        const user = await userRegistrationService(
             name,
             email,
             password,
@@ -22,4 +22,32 @@ const userRegistration = async (req,res) => {
     }
 }
 
-export default userRegistration
+const userLoginController = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const { user, token } = await userLoginService(email, password);
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "lax",
+            maxAge: 24 * 60 * 60 * 1000
+        });
+
+        res.status(200).json({
+            success: true,
+            message: "Login successful",
+            user,
+            token
+        });
+
+    } catch (error) {
+        res.status(401).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+export {userRegistrationController, userLoginController}
