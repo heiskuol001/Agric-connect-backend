703738d0ea93c66bc5cd889f40ccb30695d9f641
Kuol Magok <heiskuol001@gmail.com>
2026-07-30 21:01:57 +0300

    feat: implement server initialization with database connection and environment configuration

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import app from "./app.js";
+import { configDotenv } from "dotenv";
+import connectDB from "./config/db.js";
+
+configDotenv();
+
+const PORT = process.env.PORT || 8000;
+
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+
+    } catch (error) {
+        console.log("Server failed to start:", error.message);
+    }
+};
+
+startServer();
