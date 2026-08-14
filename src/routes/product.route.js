bfcef94dfedb2d973822bba665178480146de773
Kuol Magok <heiskuol001@gmail.com>
2026-08-15 00:16:44 +0300

    feat: add authentication middleware to product routes for enhanced security

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import {upload} from '../middlewares/middlewares.js'
-import {addProductController, deleteProductController, getProductController} from '../controllers/product.controller.js'
+import { addProductController, deleteProductController, getProductController } from '../controllers/product.controller.js'
+import authMiddleware from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
-router.post('/api/add', upload.single('image'), addProductController)
-router.delete('/api/delete/:id', deleteProductController)
+router.post('/api/add', upload.single('image'), authMiddleware, addProductController)
+router.delete('/api/delete/:id', authMiddleware, deleteProductController)
 router.get('/api/fetch', getProductController)
 
 export default router
