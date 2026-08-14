e7bfc25f53b1a2ebdfb86ef2026f59648881e2cb
Kuol Magok <heiskuol001@gmail.com>
2026-08-14 22:10:34 +0300

    feat: add login rate limiting middleware to user login route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import {userRegistrationController, userLoginController }from '../controllers/user.controller.js'
+import { userRegistrationController, userLoginController } from '../controllers/user.controller.js'
+import {loginLimiter} from '../middlewares/middlewares.js'
 
 const router = express.Router()
 
 
 router.post('/api/auth/register', userRegistrationController)
-router.post('/api/auth/login', userLoginController)
+router.post('/api/auth/login',loginLimiter, userLoginController)
 
 
 export default router
