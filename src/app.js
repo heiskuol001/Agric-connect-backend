0a80f0934f22275538f1fb61280898681701a4e5
Kuol Magok <heiskuol001@gmail.com>
2026-08-26 00:51:10 +0300

    refactor: reorganize middleware usage and improve security with helmet and cookie-parser

--- src/app.js
+++ src/app.js
@@ -4,14 +4,22 @@ import userRoutes from '../src/routes/user.route.js'
 import productRoutes from '../src/routes/product.route.js'
 import { generalLimiter } from '../src/middlewares/middlewares.js'
 import redisClient from './config/redis.js'
+import helmet from 'helmet'
+import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors())
+app.use(helmet())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(generalLimiter)
-
+app.use(cookieParser())
 app.use('/user', userRoutes)
 app.use('/product', productRoutes)
 
