c9105e3c80a9f292ea93d84212f4aa9512aecc1a
Kuol Magok <heiskuol001@gmail.com>
2026-08-26 00:52:03 +0300

    feat: add logout endpoint to user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from 'express'
-import { userRegistrationController, userLoginController } from '../controllers/user.controller.js'
+import { userRegistrationController, userLoginController, userLogOutController } from '../controllers/user.controller.js'
 import {loginLimiter} from '../middlewares/middlewares.js'
 
 const router = express.Router()
 
 
 router.post('/api/auth/register', userRegistrationController)
-router.post('/api/auth/login',loginLimiter, userLoginController)
+router.post('/api/auth/login', loginLimiter, userLoginController)
+router.post('/api/auth/logout', userLogOutController)
 
 
 export default router
