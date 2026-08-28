30babce27453597305fb0259a57e7b0293c5eb50
Kuol Magok <heiskuol001@gmail.com>
2026-08-28 15:46:52 +0300

    feat: add notification route to product API for fetching notifications

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {upload} from '../middlewares/middlewares.js'
-import { addProductController, deleteProductController, getProductController,getFarmerProductCount } from '../controllers/product.controller.js'
+import { addProductController, deleteProductController, getProductController,getFarmerProductCount,getNotificationController } from '../controllers/product.controller.js'
 import authMiddleware from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
@@ -9,5 +9,6 @@ router.post('/api/add', upload.single('image'), authMiddleware, addProductContro
 router.delete('/api/delete/:id', authMiddleware, deleteProductController)
 router.get('/api/fetch', authMiddleware, getProductController)
 router.get('/api/product/count', authMiddleware, getFarmerProductCount)
+router.get('/api/product/notifications', authMiddleware, getNotificationController)
 
 export default router
