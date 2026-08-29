53024c7e09c89d4f8d96874b7045222c36919ab3
Kuol Magok <heiskuol001@gmail.com>
2026-08-30 02:29:03 +0300

    feat: add getCurrentUser route for fetching current user details

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import { userRegistrationController, userLoginController, userLogOutController } from '../controllers/user.controller.js'
-import {loginLimiter} from '../middlewares/middlewares.js'
+import { userRegistrationController, userLoginController, userLogOutController,getCurrentUser } from '../controllers/user.controller.js'
+import { loginLimiter } from '../middlewares/middlewares.js'
+import authMiddleware from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
@@ -8,6 +9,7 @@ const router = express.Router()
 router.post('/api/auth/register', userRegistrationController)
 router.post('/api/auth/login', loginLimiter, userLoginController)
 router.post('/api/auth/logout', userLogOutController)
+router.get("/api/auth/me", authMiddleware, getCurrentUser)
 
 
 export default router
