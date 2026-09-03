c3b9f0b694d43e9c4f654a6e785c1ce9d2bdbf50
Kuol Magok <heiskuol001@gmail.com>
2026-09-04 01:33:16 +0300

    fix: remove login limiter from login route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -7,7 +7,7 @@ const router = express.Router()
 
 
 router.post('/api/auth/register', userRegistrationController)
-router.post('/api/auth/login', loginLimiter, userLoginController)
+router.post('/api/auth/login', userLoginController)
 router.post('/api/auth/logout', userLogOutController)
 router.get("/api/auth/me", authMiddleware, getCurrentUser)
 
