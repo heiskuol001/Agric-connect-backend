ded047c67567564ffac9469ab67b38d8c6a1f6c4
Kuol Magok <heiskuol001@gmail.com>
2026-09-04 01:33:34 +0300

    feat: add getMarketProductsController to product routes

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {upload} from '../middlewares/middlewares.js'
-import { addProductController, deleteProductController, getProductController,getFarmerProductCount,getNotificationController } from '../controllers/product.controller.js'
+import { addProductController, deleteProductController, getProductController,getFarmerProductCount,getNotificationController,getMarketProductsController } from '../controllers/product.controller.js'
 import authMiddleware from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
@@ -10,5 +10,6 @@ router.delete('/api/delete/:id', authMiddleware, deleteProductController)
 router.get('/api/fetch', authMiddleware, getProductController)
 router.get('/api/product/count', authMiddleware, getFarmerProductCount)
 router.get('/api/product/notifications', authMiddleware, getNotificationController)
+router.get('/api/market/products', getMarketProductsController)
 
 export default router
