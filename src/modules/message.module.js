453e2f30137f9c9714551ccbe035fbe7830c24c8
Kuol Magok <heiskuol001@gmail.com>
2026-09-04 14:46:25 +0300

    feat: create message schema for handling messages in conversations

---
+++ src/modules/message.module.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const messageSchema = new mongoose.Schema({
+    conversationId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Conversation',
+        required: true
+    },
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+},
+    {
+    timestamps: true
+    })
+
+const Message = mongoose.model('Message', messageSchema)
